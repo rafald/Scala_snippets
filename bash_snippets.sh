02aede3730bb7a4a8d60b0dd202dc1c33c6304de
rafald <rdzbek@gmail.com>
2016-10-19 12:56:13 +0200

    Create bash_snippets.sh

---
+++ bash_snippets.sh
@@ -0,0 +1,5 @@
+# install scala kernel in jupyter
+curl -L -o jupyter-scala https://git.io/vrHhi && chmod +x jupyter-scala && ./jupyter-scala && rm -f jupyter-scala
+# https://github.com/alexarchambault/jupyter-scala
+
+
